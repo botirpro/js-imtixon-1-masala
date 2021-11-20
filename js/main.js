bc41127f7188c64d533065580761d45bbeb57471
botirpro <b.unarov@yandex.ru>
2021-11-20 11:21:28 +0500

    firstcommit

---
+++ js/main.js
@@ -0,0 +1,18 @@
+var array=["Jazz","Blues"]
+console.log(array);
+
+
+
+array.push("Rock-n-roll")
+console.log(array);
+
+
+array.splice(1,1,"Classics")
+console.log(array);
+
+array.shift(0)
+console.log(array);
+
+
+array.unshift("Rap","Reggae")
+console.log(array);
